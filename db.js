4fa3a3f31ca4c98b84d3606d8bf14fee3e30259d
Robert Szaloki <robert.szaloki@euedge.com>
2015-01-21 18:16:10 +0100

    following changes

--- db.js
+++ db.js
@@ -3,14 +3,15 @@ var janeDb = new Dexie("Jane");
 
 var recordDef = { records: "id,version,currentDate",
                   commits: "version",
-                  profiles: "profile" };
+                  profiles: "profile",
+                  changes: "version, type, table" };
 
 johnDb.version(1).stores(recordDef);
 
 johnDb.open();
 
-/*
 janeDb.version(1).stores(recordDef);
 
 janeDb.open();
-*/
+
+
--- sync.js
+++ sync.js
@@ -16,10 +16,44 @@ Clock.prototype.increment = function( id ) {
   this[id] = this[id] ? this[id]+1 : 1;
 };
 
+Clock.allKeys = function (a, b){
+  var last = null;
+  return Object.keys(a)
+    .concat(Object.keys(b))
+    .sort()
+    .filter(function(item) {
+      // to make a set of sorted keys unique, just check that consecutive keys are different
+      var isDuplicate = (item == last);
+      last = item;
+      return !isDuplicate;
+    });
+};
+
+Clock.compare = function(a, b) {
+  var isGreater = false,
+      isLess = false;
+
+  // allow this function to be called with objects that contain clocks, or the clocks themselves
+  if(a.clock) a = a.clock;
+  if(b.clock) b = b.clock;
+
+  Clock.allKeys(a, b).forEach(function(key) {
+    var diff = (a[key] || 0) - (b[key] || 0);
+    if(diff > 0) isGreater = true;
+    if(diff < 0) isLess = true;
+  });
+
+  if(isGreater && isLess) return 0;
+  if(isLess) return -1;
+  if(isGreater) return 1;
+  return 0; // neither is set, so equal
+};
 
 
-function Profile(db) {
 
+function Profile(db, tables) {
+
+  this.tables = tables;
   this.db = db;
 
   this.id = GUID();
@@ -30,7 +64,9 @@ function Profile(db) {
   }
 }
 
-Profile.prototype.load = function() {
+Profile.prototype.init = function() {
+
+  this.addHooks();
 
   return this.db.profiles.get("me").then( (function( profile ) {
   var db = this.db;
@@ -45,18 +81,24 @@ Profile.prototype.load = function() {
   }).bind(this) );
 };
 
-Profile.prototype.addHooks = function( tables ) {
+Profile.prototype.addHooks = function() {
   var profile = this;
-  tables.forEach((function(table){
-    profile.db[table].hook("creating", (function(pK, obj, transaction) {
+  this.tables.forEach((function(table){
+    profile.db[table].hook("creating", (function(currentTable, pK, obj, transaction) {
+      var pKey = pK || GUID();
       obj.version = this.version;
-      return GUID();
-    }).bind(profile));
-    profile.db[table].hook("updating", (function(modifications, pK, obj, transaction) {
+      profile.db.changes.add({ version:this.version, type:"create", table:currentTable, objectId:pKey });
+      return pKey;
+    }).bind(profile, table));
+    profile.db[table].hook("updating", (function(currentTable, modifications, pK, obj, transaction) {
+      //transaction.table("changes").add({ version:this.version, type:"update", table:currentTable, objectId:pKey });
       return {
         version:this.version
       };
-    }).bind(profile));
+    }).bind(profile, table));
+    profile.db[table].hook("deleting", (function(currentTable, pK, obj, transaction) {
+      //transaction.table("changes").add({ version:this.version, type:"delete", table:currentTable, objectId:pK });
+    }).bind(profile, table));
   }));
 };
 
@@ -87,32 +129,51 @@ Profile.prototype.commit = function() {
   }).bind(this));
 };
 
+Profile.prototype.getChangedRecords = function( clock ) {
+  var db = this.db;
+  return false;
+  function f(item){
+
+  }
+
+  return db.commits.toArray(function(commits) {
+    commits.sort(Clock.compare);
+  });
+};
+
 
 var john = new Profile(johnDb,["records"]);
-john.load().then( function(val) {
-  var db = john.db;
-  console.log(val);
-  john.addHooks(["records"]);
+var jane = new Profile(janeDb,["records"]);
 
-  db.records.add({ currentDate:1, amount:10 })
-    .then(john.commit.bind(john))
-    .then(function(){
-      return db.transaction("rw",db.records,function(){
-        db.records.add({ currentDate:2, amount:20 });
-        db.records.add({ currentDate:3, amount:30 });
+Promise.all([
+  johnDb.records.clear(),
+  johnDb.profiles.clear(),
+  johnDb.commits.clear(),
+  johnDb.changes.clear(),
+  janeDb.records.clear(),
+  janeDb.profiles.clear(),
+  janeDb.commits.clear(),
+  janeDb.changes.clear(),
+  john.init().then( function( john ) {
+    var db = john.db;
+    console.log( "john initialized" );
+
+    return db.records.add({ currentDate:1, amount:10 })
+      .then(john.commit.bind(john))
+      .then(function(){
+        return db.transaction("rw",db.records,function(){
+          db.records.add({ currentDate:2, amount:20 });
+          db.records.add({ currentDate:3, amount:30 });
+        });
       });
-    })
-    .then(john.commit.bind(john));
+  }),
+  jane.init()
+]).then(function(){
+    console.log("end");
+//  john.getChangedRecords(jane.clock);
+}).catch(function(v){
+  console.log(v);
 });
 
 
-/*
-var jane = new Profile(janeDb.records);
-
-jane.sync(johnId);
-
-janeDb.records.add({ currentDate:1, amount:-10 });
-janeDb.records.add({ currentDate:3, amount:20 });
-janeDb.records.add({ currentDate:5, amount:-5 });
-*/
 
